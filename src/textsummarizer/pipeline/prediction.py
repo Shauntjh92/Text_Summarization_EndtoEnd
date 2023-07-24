fa67b825ad490029bfbf513af0191a35736c508b
Shauntjh92 <shauntjh92@gmail.com>
2023-07-24 21:47:29 +0800

    fixed error

--- src/textsummarizer/pipeline/prediction.py
+++ src/textsummarizer/pipeline/prediction.py
@@ -5,12 +5,12 @@ from transformers import pipeline
 
 class PredictionPipeline:
     def __init__(self):
-        self.config =  ConfigurationManager.get_model_evaluation_config()
+        self.config =  ConfigurationManager().get_model_evaluation_config()
 
     
     def predict(self,text):
-        tokenizer = AutoTokenizer.from_pretrained(self,self.config.tokenizer_path)
-        gen_kwargs = {"length penalty": 0.8, "num_beams":8, "max_length":128}
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
+        gen_kwargs = {"num_beams":8, "max_length":128}
 
         pipe = pipeline("summarization",model= self.config.model_path,tokenizer=tokenizer)
 
